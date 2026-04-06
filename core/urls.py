1b90633e2af368ece477607e65a3c0e07d1574c6
Serge Lionel LOKO <monsieurlesage@Air-de-Serge.chello.hu>
2026-04-06 15:03:10 +0200

    Fix: Ajout de l'import auth_views dans urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views # <-- L'IMPORTATION CORRIGÉE ICI
 
-from hub import views  # Import global pour utiliser views.nom_de_la_fonction
+from hub import views
 
 urlpatterns = [
     # Administration
@@ -31,7 +32,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', views.login_view, name='login'),
 
-    # Routes pour la réinitialisation du mot de passe
+    # Routes pour la réinitialisation du mot de passe (Utilisent auth_views importé plus haut)
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
