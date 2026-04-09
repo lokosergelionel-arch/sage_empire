7c0691e8ea769085b7dc5261aba8724796474026
Serge Lionel LOKO <monsieurlesage@MacBook-Air-de-Serge.local>
2026-04-09 11:42:05 +0200

    Connexion finale à la base de données immortelle Neon

--- core/settings.py
+++ core/settings.py
@@ -58,13 +58,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
-# --- BASE DE DONNÉES ---
-# SQLite est parfait pour tes tests avec l'équipe
+import dj_database_url
+
+# On garde SQLite par défaut en local, mais on utilise PostgreSQL sur Render
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # --- VALIDATION MOTS DE PASSE ---
