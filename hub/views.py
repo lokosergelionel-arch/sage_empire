c15dca9e41862a6c4f270a5ec0c1dff1b235d668
Serge Lionel LOKO <monsieurlesage@Air-de-Serge.chello.hu>
2026-05-01 13:15:03 +0200

    fix: rename all password reset templates to avoid Django default conflict

--- hub/views.py
+++ hub/views.py
@@ -237,19 +237,19 @@ def renvoyer_email_verification(request):
 # ===================== PASSWORD RESET EXTERNES =====================
 class CustomPasswordResetView(PasswordResetView):
     template_name = 'registration/styliste_password_reset.html'
-    email_template_name = 'registration/password_reset_email.html'
+    email_template_name = 'registration/styliste_password_reset_email.html'
     subject_template_name = 'registration/password_reset_subject.txt'
     success_url = reverse_lazy('styliste_password_reset_done')
 
 
 class CustomPasswordResetDoneView(PasswordResetDoneView):
-    template_name = 'registration/password_reset_done.html'
+    template_name = 'registration/styliste_password_reset_done.html'
 
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
-    template_name = 'registration/password_reset_confirm.html'
+    template_name = 'registration/styliste_password_reset_confirm.html'
     success_url = reverse_lazy('styliste_password_reset_complete')
 
 
 class CustomPasswordResetCompleteView(PasswordResetCompleteView):
-    template_name = 'registration/password_reset_complete.html'
+    template_name = 'registration/styliste_password_reset_complete.html'
