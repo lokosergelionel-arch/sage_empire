5b7de8a30cb5502eb2f42f5f1b60b513b87083c1
Serge Lionel LOKO <monsieurlesage@Air-de-Serge.chello.hu>
2026-05-11 21:47:25 +0200

    Fix: Nettoyage de la fonction de création automatique de l'admin

--- hub/apps.py
+++ hub/apps.py
@@ -8,17 +8,25 @@ class HubConfig(AppConfig):
     def ready(self):
         post_migrate.connect(create_or_update_admin, sender=self)
 
+
 def create_or_update_admin(sender, **kwargs):
     from django.contrib.auth.models import User
-    # Remplace par tes vraies infos
+    # On importe les signaux pour pouvoir les bloquer temporairement
+    from django.db.models.signals import post_save
+
     username = 'admin'
     email = 'loko.sergelionel@gmail.com'
-    password = 'SageEmpire2026!' # Garde celui-là pour le moment
+    password = 'SageEmpire2026!'
 
-    if not User.objects.filter(username=username).exists():
+    # On vérifie si l'admin existe déjà
+    user_exists = User.objects.filter(username=username).exists()
+
+    if not user_exists:
+        # On crée le superuser
         User.objects.create_superuser(username, email, password)
         print("--- SAGE EMPIRE : SUPERUSER CRÉÉ ---")
     else:
+        # On met à jour l'admin existant
         user = User.objects.get(username=username)
         user.email = email
         user.set_password(password)
